22826e9e715d52c637bc8825affcc3c638907d05
Myung Joon Kang <contact@myungjoon.com>
2025-09-20 20:39:07 -0700

    Initial Commit

---
+++ Accelab/AccelabApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccelabApp.swift
+//  Accelab
+//
+//  Created by Myung Joon Kang on 2025-09-20.
+//
+
+import SwiftUI
+
+@main
+struct AccelabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
