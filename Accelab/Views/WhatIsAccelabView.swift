2a7a95924a40121fa59e6bab3a1345d511e35c52
Myung Joon Kang <contact@myungjoon.com>
2025-09-23 18:32:44 -0700

    fix: address minor issues on WhatIsAccelabView

--- Accelab/Views/WhatIsAccelabView.swift
+++ Accelab/Views/WhatIsAccelabView.swift
@@ -14,11 +14,11 @@ struct WhatIsAccelabView: View {
         NavigationStack {
             ScrollView {
                 VStack(alignment: .leading, spacing: 15) {
-                    Text("Have you ever done a lab at school that requires you to collect time-distance data of a cart sliding down an air track? If you have, you would definately agree that it is a very tedious task. Setting up the air track at the right angle, recording a video with a stopwatch, writing down the distance the cart travelled at a certain point in time, and repeating this process... That's where Accelab comes in! Using your iPhone's sensors, Accelab can:")
+                    Text("Have you ever done a lab at school that requires you to collect distance-time data of a cart sliding down an air track? If you have, you would definitely agree that it is a very tedious task. Setting up the air track at the right angle, recording a video with a stopwatch, writing down the distance the cart travelled at a certain point in time, and repeating this process... That's where Accelab comes in! Using your iPhone's sensors, Accelab can:")
                     
                     VStack(alignment: .leading, spacing: 5) {
                         Text("⋅ Make sure that your air track is set up with the angle you chose")
-                        Text("⋅ Collect the time-distance data of your cart as it travels down the air track")
+                        Text("⋅ Collect the distance-time data of your cart as it travels down the air track")
                         Text("⋅ Create a CSV file with the collected data so it's easy for analysis")
                     }
                     
